d161684e46e5ba3d53a831755eb8ee62f2a22467
farza <farzanrichu610@gmail.com>
2025-08-14 01:51:01 +0530

    updated only for table ui to be viewed

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -1,9 +1,15 @@
+import Table from "../Components/Table.tsx";
+
 function Hero() {
     return (
         <>
-            <h1 className="flex flex-col justify-center text-3xl font-bold items-center h-screen">
-                HEyy this is here
-            </h1>
+            {/*<h1 className="flex flex-col justify-center text-3xl font-bold items-center h-screen">*/}
+            {/*   Project Helper*/}
+            {/*</h1>*/}
+            {/*<h2>*/}
+            {/*    Struggling to make a gant chart or table,you have reahced the ight place*/}
+            {/*</h2>*/}
+            <Table/>
         </>
     );
 }
