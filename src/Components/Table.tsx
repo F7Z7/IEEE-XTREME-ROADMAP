c06c905ca3b48ca5cdddadf319a52d604d616fb8
farza <farzanrichu610@gmail.com>
2025-08-14 02:09:27 +0530

    centering the div

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -63,7 +63,7 @@ const TablePage: React.FC = () => {
         });
     }
     return (
-        <div className="p-6">
+        <div className="p-6 flex flex-col justify-center items-center">
             <h1 className="text-2xl font-bold mb-4">Literature Review Table</h1>
 
             {/* Form */}
