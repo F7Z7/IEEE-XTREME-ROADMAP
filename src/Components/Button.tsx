786a418b57b6c9212fecc35b1aa99b30aef51e0e
farza <farzanrichu610@gmail.com>
2025-08-14 01:48:15 +0530

    cursor pointer

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 function Button({onclick, text, bgColor}: ButtonProps) {
     return (
         <div>
-            <button onClick={onclick} className={`${bgColor} text-white px-4 py-2 rounded `}>
+            <button onClick={onclick} className={`${bgColor} text-white px-4 py-2 rounded cursor-pointer`}>
                 {text}
             </button>
         </div>
